af859395e6720f8d35b9473a55900b2b3c7c2586
blackmouse572 <Ngocvlqt1995@gmail.com>
2022-04-26 20:12:25 +0700

    Add font family, custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,21 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    fontFamily:{
-      'sans': ['IBM Plex Sans','Montserrat', 'sans-serif'],
-      'serif': ['Playfair Display','Lora', 'serif'],
-      'mono':['Ubuntu Mono', 'Roboto Mono', 'monospace']
+    fontFamily: {
+      sans: ["IBM Plex Sans", "Montserrat", "sans-serif"],
+      serif: ["Playfair Display", "Lora", "serif"],
+      mono: ["Ubuntu Mono", "Roboto Mono", "monospace"],
     },
-    extend: {},
+    extend: {
+      colors: {
+        "red-primary": "#C03540",
+        "red-secondary": "#CBB3647",
+        "red-highlight": "#F43F5E",
+        "light-gray": "#FFFFFF33",
+        "dark-gray": "#262626",
+        "white-bg": "#EBE7E0",
+      },
+    },
   },
   plugins: [],
-}
+};

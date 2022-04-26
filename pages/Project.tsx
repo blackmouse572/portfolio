d6c82ba91b8082f1fb422312e230e2282aa9f929
blackmouse572 <Ngocvlqt1995@gmail.com>
2022-04-26 20:14:26 +0700

    Init Project Event

--- pages/Project.tsx
+++ pages/Project.tsx
@@ -1,12 +1,24 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import React from "react";
+import Navbar from "../components/Navbar/Navbar";
 
 function Project() {
   return (
     <div>
-        <Navbar/>
-        Project</div>
-  )
+      <Navbar />
+      <div className="lg:px-8 pt-24 lg:pt-36 min-h-screen  md:px-5 relative">
+        <div className="max-w-7xl mx-auto">
+          <div className="w-full h-full flex items-end px-1 fade-in">
+            <div className="h-full pb-6 px-4 lg:px-0">
+              <h3 className="floatinAnimation top-0 font-sans text-4xl lg:text-6xl uppercase  font-black  tracking-tight">
+                Project
+                <span className="opacity-10 text-black pl-4">//</span>
+              </h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Project
+export default Project;

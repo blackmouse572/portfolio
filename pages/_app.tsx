2b9da6b8d9d049752d0f3df6b7c821c3a56dc5f8
blackmouse572 <Ngocvlqt1995@gmail.com>
2022-08-22 16:46:35 +0700

    Refactor: add FA icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,31 +1,35 @@
-import "../styles/globals.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 import type { AppProps } from "next/app";
-import MouseContextProvider from "../components/Cursor/cursorContext";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import MouseContextProvider from "../components/Cursor/cursorContext";
 import Loading from "../components/Loading/Loading";
+import "../styles/globals.css";
+config.autoAddCss = false;
+
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
-  const [loading, setLoading]= useState(false);
-  useEffect(()=>{
-    const handlerStart = (url:string)=> {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const handlerStart = (url: string) => {
       url != router.pathname ? setLoading(true) : setLoading(false);
-    }
-    const handlerComplete =  (url:string)=> {
+    };
+    const handlerComplete = (url: string) => {
       setLoading(false);
-    }
-    
+    };
+
     router.events.on("routeChangeStart", handlerStart);
     router.events.on("routeChangeComplete", handlerComplete);
     router.events.on("routeChangeError", handlerComplete);
-  },[router])
+  }, [router]);
   return (
     <MouseContextProvider>
       <Head>
         <title> NTN Portfolio </title>
       </Head>
-      <Loading loading={loading}/>
+      <Loading loading={loading} />
       <Component {...pageProps} />
     </MouseContextProvider>
   );

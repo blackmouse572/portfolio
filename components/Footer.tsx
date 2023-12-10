753df40c34a3027bced6189aa948c1771da3df09
blackmouse572 <Ngocvlqt1995@gmail.com>
2023-12-10 16:23:42 +0700

    feat: simplefy footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,51 +1,9 @@
-import { motion } from "framer-motion";
 function Footer() {
   return (
-    <section className="my-5 md:my-10 lg:my-12">
-      <motion.div
-        initial={"hidden"}
-        whileInView={"visible"}
-        viewport={{
-          once: true,
-        }}
-        variants={{
-          hidden: {
-            opacity: 0,
-            width: 0,
-          },
-          visible: {
-            opacity: 1,
-            width: "100%",
-            transition: {
-              duration: 0.5,
-              ease: "easeInOut",
-            },
-          },
-        }}
-        className="border border-black bg-black"></motion.div>
-      <motion.p
-        initial={"hidden"}
-        whileInView={"visible"}
-        viewport={{
-          once: true,
-        }}
-        variants={{
-          hidden: {
-            opacity: 0,
-            x: -100,
-          },
-          visible: {
-            opacity: 1,
-            x: 0,
-            transition: {
-              duration: 0.5,
-              ease: "easeInOut",
-            },
-          },
-        }}
-        className="text-center font-mono text-md tracking-wide">
+    <section className="md:py-10 lg:py-12 bg-white-bg border-t border-black">
+      <p className="text-center font-mono text-md tracking-wide py-4">
         Copyrights @ 2022 Nguyen Tuan Ngoc. All rights reserved.
-      </motion.p>
+      </p>
     </section>
   );
 }

c659337bfb9c3629caac0ffb5c0cecb638f542a1
Jaden.Nguyen <jaden.nguyen@avepoint.com>
2024-01-30 12:25:58 +0700

    temp: remove pretty code plugin

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,35 +1,38 @@
-import { ComputedFields, defineDocumentType, makeSource } from 'contentlayer/source-files';
-import rehypeAutolinkHeadings from 'rehype-autolink-headings';
-import rehypePrettyCode from 'rehype-pretty-code';
-import rehypeSlug from 'rehype-slug';
-import remarkGfm from 'remark-gfm';
+import {
+  ComputedFields,
+  defineDocumentType,
+  makeSource,
+} from "contentlayer/source-files";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeSlug from "rehype-slug";
+import remarkGfm from "remark-gfm";
 
 const computedFields: ComputedFields = {
   slug: {
-    type: 'string',
-    resolve: (doc) => doc._raw.sourceFileName.replace(/\.mdx$/, ''),
+    type: "string",
+    resolve: (doc) => doc._raw.sourceFileName.replace(/\.mdx$/, ""),
   },
 };
 
 export const Project = defineDocumentType(() => ({
-  name: 'Project',
+  name: "Project",
   filePathPattern: `**/*.mdx`,
-  contentType: 'mdx',
+  contentType: "mdx",
   fields: {
     title: {
-      type: 'string',
+      type: "string",
       required: true,
     },
     thumbnail: {
-      type: 'string',
+      type: "string",
       required: true,
     },
     description: {
-      type: 'string',
+      type: "string",
       required: true,
     },
     date: {
-      type: 'date',
+      type: "date",
       default: new Date().toISOString(),
     },
   },
@@ -37,35 +40,17 @@ export const Project = defineDocumentType(() => ({
 }));
 
 export default makeSource({
-  contentDirPath: 'content/projects',
+  contentDirPath: "content/projects",
   documentTypes: [Project],
   mdx: {
     remarkPlugins: [remarkGfm],
     rehypePlugins: [
       rehypeSlug,
       [
-        rehypePrettyCode as any, //prevent type error
-        {
-          theme: 'one-dark-pro',
-          keepBackground: false,
-          grid: true,
-          filterMetaString: (st: string) => st.replace(/filename="[^"]*"/, ''),
-
-          onVisitLine(node: any) {
-            // Prevent lines from collapsing in `display: grid` mode, and
-            // allow empty lines to be copy/pasted
-            if (!node) return;
-            if (node.children.length === 0) {
-              node.children = [{ type: 'text', value: '' }];
-            }
-          },
-        },
-      ],
-      [
         rehypeAutolinkHeadings,
         {
           properties: {
-            className: 'anchor',
+            className: "anchor",
           },
         },
       ],
